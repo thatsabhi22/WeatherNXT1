22cb375426cdc83de8a057694d0885ee260938e5
abhijeet <redefinedabhi2@gmail.com>
2023-07-10 08:43:08 +0530

    Update AppModule.kt
    Added AppModule for providing Retrofit Instance through Dagger Hilt

--- app/src/main/java/com/theleafapps/pro/weathernxt1/di/AppModule.kt
+++ app/src/main/java/com/theleafapps/pro/weathernxt1/di/AppModule.kt
@@ -1,4 +1,29 @@
 package com.theleafapps.pro.weathernxt1.di
 
+import com.theleafapps.pro.weathernxt1.api.WeatherApiService
+import com.theleafapps.pro.weathernxt1.utils.Constants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 class AppModule {
+
+    @Provides
+    fun provideBaseUrl() = Constants.BASE_URL
+
+    @Provides
+    @Singleton
+    fun provideRetrofitInstance(BASE_URL: String): WeatherApiService =
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(WeatherApiService::class.java)
+
 }

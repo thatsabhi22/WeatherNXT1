611b1acb211950b452a034d707b8fb575bb70012
abhijeet <redefinedabhi2@gmail.com>
2023-07-10 13:31:07 +0530

    Update WeatherRepository.kt
    Added Repository methods for query to api and db, also insert

--- app/src/main/java/com/theleafapps/pro/weathernxt1/repository/WeatherRepository.kt
+++ app/src/main/java/com/theleafapps/pro/weathernxt1/repository/WeatherRepository.kt
@@ -1,4 +1,21 @@
 package com.theleafapps.pro.weathernxt1.repository
 
-class WeatherRepository {
+import androidx.lifecycle.LiveData
+import com.theleafapps.pro.weathernxt1.api.WeatherApiService
+import com.theleafapps.pro.weathernxt1.db.WeatherDataDAO
+import com.theleafapps.pro.weathernxt1.models.WeatherInfo
+import javax.inject.Inject
+
+class WeatherRepository @Inject
+constructor(private val weatherApiService: WeatherApiService,
+            private val weatherDataDao: WeatherDataDAO) {
+
+    suspend fun getWeather(cityName: String, apiKey: String, units: String) =
+        weatherApiService.getWeatherData(cityName,apiKey,units)
+
+    suspend fun insertWeatherInfo(weatherInfo: WeatherInfo) =
+        weatherDataDao.insertWeatherInfo(weatherInfo)
+
+    fun getWeatherInfoFromDB(wId:Int): LiveData<WeatherInfo> =
+        weatherDataDao.getLastCityWeatherInfo(wId)
 }
